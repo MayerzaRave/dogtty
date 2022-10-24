5d9b761b87ec253c4dffffa40f4e7c825636c2b9
Juan Camilo Ramirez Velez <juancrv28@gmail.com>
2022-10-24 10:17:21 -0500

    week calendar display a care

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # start_date = params.fetch(:schedule, Date.today).to_date
+    # @cares = Care.where(schedule: start_date.beginning_of_week..start_date.end_of_week)
+    @cares = Care.all
   end
 end
