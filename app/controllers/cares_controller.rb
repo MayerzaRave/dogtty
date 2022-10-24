18f360e02fc56aad535cd81d06d6f0765343e7fd
yeison valencia <yeison.valencia42@gmail.com>
2022-10-24 10:03:52 -0500

    Merge branch 'master' into user-view

--- app/controllers/cares_controller.rb
+++ app/controllers/cares_controller.rb
@@ -1,5 +1,6 @@
 class CaresController < ApplicationController
   before_action :set_pets, only: :new
+  before_action :set_care, only: %i[new create]
 
   def new
     @care = Care.new
@@ -11,7 +12,7 @@ class CaresController < ApplicationController
     @pets_id[1, @pets_id.size].each do |id|
       @care = Care.new(care_params)
       @care.pet = Pet.find(id.to_i)
-      @care.save
+      @care.save 
     end
     redirect_to root_path, notice: 'care was successfully created!'
   end
@@ -24,5 +25,10 @@ class CaresController < ApplicationController
 
   def care_params
     params.require(:care).permit(:pet_id, :category, :schedule, :status, :title, :details)
+  end
+
+  def set_care
+    @care = Care.new
+    @care.title = params[:care]
   end
 end
